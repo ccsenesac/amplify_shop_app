de116ce897a577d71f1763503ba87fdbed7516d2
ccsenesac <ccsenesac@gmail.com>
2022-11-28 21:00:07 -0600

    Message

--- App.js
+++ App.js
@@ -1,12 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Amplify } from 'aws-amplify';
+//import { Authenticator } from '@aws-amplify/ui-react';
+import UselessTextInput from './src/Components/TextInput';
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
+import awsExports from './src/aws-exports';
+// import '@aws-amplify/ui-react/styles.css';
+
+
+Amplify.configure(awsExports);
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+      <UselessTextInput></UselessTextInput>
+      <Button
+        title= "Click Here"
+        color= "#928482"
+        onPress={() => Alert.alert("HI")}>
+      </Button>
+    </View>
+  );
+}
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="A"
+        component={HomeScreen}
+        options={{ tabBarLabel: 'Home!' }}
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+function ProgressScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Progress!</Text>
+    </View>
+  );
+}
+
+function MiscScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Misc!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+const SettingsStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Progress" component={ProgressScreen} />
+        <Tab.Screen name="Misc" component={MiscScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
